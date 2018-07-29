60d7c0209a17e1c15f06cad248921bb3ffbfea35
jigyasa-grover <grover.jigyasa1@gmail.com>
2018-07-29 09:50:24 -0700

    Location access

--- divine/handler.py
+++ divine/handler.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import parse_qs
 
 def handle(req):
     """handle a request to the function
@@ -6,8 +7,12 @@ def handle(req):
         req (str): request body
     """
 
+    query = os.environ['Http_Query']
+    params = parse_qs(query)
+    action = params['action'][0]
+
     dirname = os.path.dirname(__file__)
-    path = os.path.join(dirname, 'html', 'index.html')
+    path = os.path.join(dirname, 'html', action + '.html')
 
     with(open(path, 'r')) as file:
         html = file.read()
