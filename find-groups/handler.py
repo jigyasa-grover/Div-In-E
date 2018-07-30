f509e710b12a5825679ab0f5145302274b448335
Shilpi Bose <shilpi296@gmail.com>
2018-07-30 09:20:56 -0700

    Fixing JSON errors

--- find-groups/handler.py
+++ find-groups/handler.py
@@ -23,7 +23,7 @@ def handle(req):
 
     category_id = meetup_categories[category]
 
-    r = requests.get("https://api.meetup.com/find/groups?desc=india%2C+women&photo-host=public&page=5&sig_id=256722888&category=10%2C16&sig=3f7d46528c7210ccc257f08ead92d0923749e912")
+    r = requests.get("https://api.meetup.com/find/groups?desc=india%2C+women&photo-host=public&page=16&sig_id=256722888&category=10%2C16&sig=353e51582bc153f8fd3325382666")
     if r.status_code != 200:
         print("Error with meetup api, expected: %d, got: %d\n" % (200, r.status_code))
         sys.exit("Error with meetup api, expected: %d, got: %d\n" % (200, r.status_code))
