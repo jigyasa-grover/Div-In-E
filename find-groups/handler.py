7d8acf9547f1eacde3a53f6ecc7bf37b5f917a88
Shilpi Bose <shilpi296@gmail.com>
2018-07-30 00:26:33 -0700

    Fetch meetups by category

--- find-groups/handler.py
+++ find-groups/handler.py
@@ -1,16 +1,32 @@
 import os
 import json
 import requests
+import sys
+from urllib.parse import parse_qs
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
-    gateway_hostname = os.getenv("gateway_hostname", "gateway")
-    r = requests.get("https://api.meetup.com/find/groups?photo-host=public&page=20&text=india&sig_id=259945822&lon=-122.19738189999998&lat=37.4715136&sig=e6b59f26bc83740c9888e15bedaa57e3b36ee7ba")
+
+    meetup_categories = {
+        "culture": "10,16",
+        "entertainment": "20,21",
+        "wellbeing" : "9,14",
+        "pride": "12",
+    }
+
+    query = os.environ['Http_Query']
+    params = parse_qs(query)
+    category = params["category"][0]
+
+    category_id = meetup_categories[category]
+
+    r = requests.get("https://api.meetup.com/find/groups?desc=india%2C+women&photo-host=public&page=5&sig_id=256722888&category=10%2C16&sig=3f7d46528c7210ccc257f08ead92d0923749e912")
     if r.status_code != 200:
         print("Error with meetup api, expected: %d, got: %d\n" % (200, r.status_code))
         sys.exit("Error with meetup api, expected: %d, got: %d\n" % (200, r.status_code))
+    
     result = r.json()
-    print(result)
-    return result
+    return "%s(%s)" % (params["callback"][0], result)
