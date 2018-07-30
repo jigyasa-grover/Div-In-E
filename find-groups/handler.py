ee579a97d016d49fc56cc0d65e74eaa94a83a493
Shilpi Bose <shilpi296@gmail.com>
2018-07-30 09:45:21 -0700

    Add meetup links

--- find-groups/handler.py
+++ find-groups/handler.py
@@ -22,8 +22,13 @@ def handle(req):
     category = params["category"][0]
 
     category_id = meetup_categories[category]
-
-    r = requests.get("https://api.meetup.com/find/groups?desc=india%2C+women&photo-host=public&page=16&sig_id=256722888&category=10%2C16&sig=353e51582bc153f8fd3325382666")
+    if category == "culture"
+        r = requests.get("https://api.meetup.com/find/groups?desc=india%2C+women&photo-host=public&page=16&sig_id=256722888&category=10%2C16&sig=353e51582bc153f8fd3325382666")
+    elif category == "wellbeing":
+        r = requests.get("https://api.meetup.com/find/groups?photo-host=public&page=16&text=women&sig_id=256722888&category=9%2C14&sig=1d0890ae2bdab37ba97e1932ee8329e7ee783caa)
+    elif category == "pride":
+        r = requests.get("https://api.meetup.com/find/groups?photo-host=public&page=16&text=women&sig_id=256722888&category=12&sig=5df30d07e1de83ea0a44696c74bfc420ac76f396")
+    
     if r.status_code != 200:
         print("Error with meetup api, expected: %d, got: %d\n" % (200, r.status_code))
         sys.exit("Error with meetup api, expected: %d, got: %d\n" % (200, r.status_code))
